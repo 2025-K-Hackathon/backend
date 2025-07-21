8e5e50a7e4becb8a36b226c8ce9009a534f7b377
suncastle023 <badabada0619@naver.com>
2025-07-22 00:31:33 +0900

    feat: 일기 기능에 AI API 연동

--- dajeong/src/main/java/com/dajeong/dajeong/dto/DiaryResponseDTO.java
+++ dajeong/src/main/java/com/dajeong/dajeong/dto/DiaryResponseDTO.java
@@ -3,6 +3,8 @@ package com.dajeong.dajeong.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.util.List;
+import com.dajeong.dajeong.entity.Diary;
 
 @Getter @Setter
 @NoArgsConstructor
@@ -10,10 +12,19 @@ public class DiaryResponseDTO {
     private Long id;
     private String date;
     private String content;
+    private String correctedText;
+    private String reply;
+    private List<String> incorrectWords;
+    private List<String> correctedWords;
 
-    public DiaryResponseDTO(Long id, String date, String content) {
-        this.id = id;
-        this.date = date;
-        this.content = content;
+    public DiaryResponseDTO(Diary d) {
+        this.id = d.getId();
+        this.date = d.getDate().toString();
+        this.content = d.getContent();
+        this.correctedText = d.getCorrectedText();
+        this.reply = d.getReply();
+        this.incorrectWords = d.getIncorrectWords();
+        this.correctedWords = d.getCorrectedWords();
     }
 }
+
